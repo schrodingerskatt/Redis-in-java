fe7acc582958ee8aa6fbbac7f32700708c56c760
schrodingerkatt <arya222aya@gmail.com>
2024-11-10 19:35:55 +0530

    added updated ping

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class Main {
   public static void main(String[] args){
@@ -13,12 +16,16 @@ public class Main {
         int port = 6379;
         try {
           serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
           clientSocket = serverSocket.accept();
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+          // Reading from the client's input stream (to adhere to the protocol)
+          BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+          String inputLine = in.readLine();
+          // Responding to the client's request
+            String response = "+PONG\r\n";
+            OutputStream out = clientSocket.getOutputStream();
+            out.write(response.getBytes());
+            out.flush();
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
